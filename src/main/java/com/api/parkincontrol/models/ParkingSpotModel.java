124a6c22d961bbcb0d691ca32695844c78a137b0
Ericsson_da_Silva <dasilva.ericsson@gmail.com>
2022-06-08 09:07:48 -0300

    Update ParkingSpotModel.java

--- src/main/java/com/api/parkincontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkincontrol/models/ParkingSpotModel.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -16,6 +17,22 @@ public class ParkingSpotModel implements Serializable {
     private UUID id;
     @Column(nullable = false, unique = true, length = 10)
     private String parkingSpotNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String LicensePlateCar;
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+    @Column(nullable = false, length = 30)
+    private String apartment;
+    @Column(nullable = false, length = 30)
+    private String block;
 
 
 }
