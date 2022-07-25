3bfba75f9c8e9fd0ef8a044bcf7eebdcd597e614
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-07-24 23:50:01 -0300

    Finally

--- src/main/java/com/api/parkingcontrol/config/security/WebSecurityConfigV2.java
+++ src/main/java/com/api/parkingcontrol/config/security/WebSecurityConfigV2.java
@@ -2,23 +2,35 @@ package com.api.parkingcontrol.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.servlet.FilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfigV2 implements FilterChain {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfigV2 {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                //.antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+                //.antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+                //.antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+                //.anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+        return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
